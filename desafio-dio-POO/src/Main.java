4b3b110238dd25ebb0830013c7e31deaae004cd8
Isaque <andradeisq1.7@gmail.com>
2024-10-14 01:00:43 -0300

    implementacao da abstracao do projeto e inicio do encapsulamento

---
+++ desafio-dio-POO/src/Main.java
@@ -0,0 +1,27 @@
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("descricao curso de Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Python");
+        curso2.setDescricao("descricao curso de Python");
+        curso2.setCargaHoraria(6);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria Html");
+        mentoria.setDescricao("descricao mentoria Html");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+    }
+}

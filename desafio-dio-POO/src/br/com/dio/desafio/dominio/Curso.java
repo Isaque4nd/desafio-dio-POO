9c266c0da32193f57ba4a5534d4d82e8d9b9c527
Isaque <andradeisq1.7@gmail.com>
2024-10-14 01:18:05 -0300

    iniciando implementacao da heranca e do polimorfismo

--- desafio-dio-POO/src/br/com/dio/desafio/dominio/Curso.java
+++ desafio-dio-POO/src/br/com/dio/desafio/dominio/Curso.java
@@ -1,27 +1,13 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
+public class Curso extends Conteudo{
     
-    private String titulo;
-    private String descricao;
     private int cargaHoraria;
 
     public Curso() {
 
     }
-    
-    public String getTitulo() {
-        return titulo;
-    }
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-    public String getDescricao() {
-        return descricao;
-    }
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
+
     public int getCargaHoraria() {
         return cargaHoraria;
     }
@@ -30,7 +16,12 @@ public class Curso {
     }
     @Override
     public String toString() {
-        return "Curso [titulo=" + titulo + ", descricao=" + descricao + ", cargaHoraria=" + cargaHoraria + "]";
+        return "Curso [titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", cargaHoraria=" + cargaHoraria + "]";
+    }
+
+    @Override
+    public double calcularXP() {
+        return XP_PADRAO * cargaHoraria;
     }
 
     
